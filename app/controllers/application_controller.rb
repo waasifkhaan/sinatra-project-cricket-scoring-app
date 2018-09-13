d5e84765c9678db3104e730b883a7aa932b83635
Wasif Khan <waasif.khaan@gmail.com>
2018-09-13 03:10:20 +0000

    add user controller and set up sign up/ login links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,21 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get "/" do
-    erb :welcome
-  end
+  get '/' do
+      erb :welcome
+    end
+
+  helpers do
+
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+end
+
 
 end
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
+  get '/signup' do
+    # if logged_in?
+    #   # redirect to "/tweets"
+    # else
+      erb :"users/signup"
+    # end
+  end
+
+  post '/signup' do
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      redirect to "/signup"
+    else
+      @user = User.create(params)
+      session[:user_id] = @user.id
+      redirect to "/tweets"
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect to "/tweets"
+    else
+      erb :"/users/login"
+    end
+  end
+
+  post '/login' do
+
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/tweets"
+
+    else
+      redirect to "/signup"
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect to "/login"
+    else
+      redirect to '/'
+    end
+  end
+
+end
