233e7135606afa59476434e34a2efd9226eb2c73
Wasif Khan <waasif.khaan@gmail.com>
2018-09-24 22:42:33 +0000

    work on team controller and player controller to add new team and player

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,12 @@
 class PlayersController < ApplicationController
 
-  get 'players/new' do
+  get '/players/new' do
     erb :"players/new"
+  end
+
+  post '/players' do
+    Player.create(name: params[:player_name], team_id: params[:team_name])
+    redirect to '/games'
+
   end
 end
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,11 @@
 class TeamsController < ApplicationController
-  get 'teams/new' do
-    binding.pry
+  get '/teams/new' do
     erb :"teams/new"
+  end
+
+  post '/teams' do
+    Team.create(name: params[:team_name], club_id: params[:club_name])
+    redirect to '/games'
+
   end
 end
