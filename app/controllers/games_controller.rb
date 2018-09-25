cc08789784d146f4164cbbeef82e53469251ee6b
Wasif Khan <waasif.khaan@gmail.com>
2018-09-25 23:11:07 +0000

    fix the dropdown for teams playing game , limited to only two

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,12 +11,11 @@ class GamesController < ApplicationController
       redirect to '/login'
     end
   end
+
   post '/games/teams' do
 
     @team_1 = Team.find_by(id: params[:teams][0])
     @team_2 = Team.find_by(id: params[:teams][1])
-
-
     erb :'/games/new1'
   end
   post '/games/teams/players' do
@@ -24,6 +23,12 @@ class GamesController < ApplicationController
     # if params[:players_1].length == 0 || params[:players_2].length == 0
     #   redirect to "/games/teams"
     # else
+
+    @two_teams = []
+    @two_teams << params[:team_1]
+    @two_teams << params[:team_2]
+
+    # binding.pry
     @players_1 = params[:players_1].map do |id|
       Player.find(id)
     end
@@ -36,8 +41,8 @@ class GamesController < ApplicationController
 
   post '/games' do
 
-        @game = Game.create(hometeam: Team.find(params[:hometeam]).name, awayteam: Team.find(params[:awayteam]).name,
-         toss: Team.find(params[:toss]).name, result: Team.find(params[:result]).name, extra_1: params[:extras_1], extra_2: params[:extras_2], total_1: params[:total_1],
+        @game = Game.create(hometeam: params[:hometeam], awayteam: params[:awayteam],
+         toss: params[:toss], result: params[:result] , extra_1: params[:extras_1], extra_2: params[:extras_2], total_1: params[:total_1],
          total_2: params[:total_2] )
 
         @scores_1 = params[:players_1].map do |player|
